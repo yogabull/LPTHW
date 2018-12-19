8c5feed71d817afa7aeb59e3b1485c9670efe8a6
yoga <you@example.com>
2018-12-18 22:57:35 -0600

    This is vim to make a message about adding these exercises. I need to figure it out better. This is vim to make a message about adding these exercises. I need to figure it out better. nges. Lines starting

---
+++ ex11.py
@@ -0,0 +1,12 @@
+print("Exercise 11: Asking Questions")
+
+# the code: (end=' ') tells python not to carraige return for the answer, but to enter the answer right after the question.
+print('How old are you?', end=' ')
+age = input ()
+print('How tall are you?', end= ' ')
+height = input()
+# This line has messed up formatting because I mhashed out the code at the end of the line.
+print('How much do you weight?' ) #end=' '
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and weigh {weight} heavy.")
