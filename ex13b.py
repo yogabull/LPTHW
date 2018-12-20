4a34b240c192e9429dfb14ff5c72e54438cf8d9f
yoga <you@example.com>
2018-12-19 23:33:56 -0600

    please use this message

--- ex13b.py
+++ ex13b.py
@@ -1,7 +1,7 @@
 #Exercise 13b Parameters, Unpacking , Variables
 #Writing additional code that uses input and argv.
 
-print('\nExercise 13 Parameters, Unpacking , Variables; additional tests')
+print('additional tests')
 
 from sys import argv
 script, = argv
@@ -17,4 +17,9 @@ print('\n{} is running the script called'.format(name), script, '\n')
 
 #NOTE: the (.format) needs to come right after the sentence. Each element called needs separation by a comma.
 
-# this push will be made with a comment after typing git push
+# Note about Git. Work on a branch. If you push to Github while working on the master it will automatically merge with the online masterself.
+# If you push a branch, it will prompt you to make duplicate the branch online.
+# When you push from a branch it will ask you to type: git push --set-upstream origin work (work is the branch's name).
+
+#test line
+print('this is a test line')
