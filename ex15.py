a6462d44b8c3ba2ac5a6ce1a16f5120def1949b7
yoga <you@example.com>
2018-12-24 17:17:14 -0600

    Working on reading files.

---
+++ ex15.py
@@ -0,0 +1,17 @@
+#Exerciase 15: Reading files
+print('Exercise 15: Reading files\nafter a new line escape sequence\n')
+
+from sys import argv
+
+script, filename = argv
+txt = open(filename)
+
+print(f"Here's your file {filename}:")
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
