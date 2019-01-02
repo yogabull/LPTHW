9e8a40b3067f388d40162048d42f127af9e2e535
yoga <you@example.com>
2019-01-02 00:34:57 -0600

    FB test code

--- speedTest.py
+++ speedTest.py
@@ -1,19 +1,30 @@
-#tuple / list speed compared
+# #tuple / list speed compared
+#
+# from time import time
+#
+# start = time() #list version
+# lst = [n for n in range (1000000)] # 1e6 element list
+# for _ in range(100): 999999 in lst #100 look-ups
+# list_time = (time() - start)
+# print(time() -start, "secs -list look-up")
+# print (list_time)
+#
+# start = time() #tuple version
+# tup = (n for n in range(1000000)) #1e6 element tuple
+# for _ in range(100): 999999 in tup # 100 look-ups
+# print(time() - start, "secs - tuple look-up")
+# tuple_time = (time() - start)
+# print(tuple_time)
+#
+# print("Searching the tuple was",list_time / tuple_time, "times faster than the list.")
 
-from time import time
 
-start = time() #list version
-lst = [n for n in range (1000000)] # 1e6 element list
-for _ in range(100): 999999 in lst #100 look-ups
-list_time = (time() - start)
-print(time() -start, "secs -list look-up")
-print (list_time)
-
-start = time() #tuple version
-tup = (n for n in range(1000000)) #1e6 element tuple
-for _ in range(100): 999999 in tup # 100 look-ups
-print(time() - start, "secs - tuple look-up")
-tuple_time = (time() - start)
-print(tuple_time)
-
-print("Searching the tuple was",list_time / tuple_time, "times faster than the list.")
+# Another test from FB group
+y = ('a', 'b', 'c')
+x = ['ab','cd']
+for i in x:
+    i.upper()
+for i in y:
+    i.upper()
+print(x)
+print(y)
