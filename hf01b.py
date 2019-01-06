b3e153419949dc496a8d909ac44cf2ef461dcddf
yoga <you@example.com>
2019-01-06 01:29:52 -0600

    more exercises

---
+++ hf01b.py
@@ -0,0 +1,11 @@
+# 99 Bottle of beer on the wall
+
+
+for i in range (99, 0, -1):
+    b = 'bottles'
+    print(i, b, "of beer on the wall.")
+    print(i, "bottles of beer.")
+    print("Take one down; pass it around")
+    print()
+    if i == 1:
+        print("Zero bottles of beer on the wall.")
